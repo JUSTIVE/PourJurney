551086ecdfefacdd63b76d0a73d9601d4c5ca647
Ben <ben399399@naver.com>
2020-03-22 23:49:40 +0900

    Finished histogram plotting

--- plot.py
+++ plot.py
@@ -1,8 +1,26 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
-data = [60.00,75.00,88.00,74.00,73.00,92.00,80.00,97.00,83.00,71.00,68.00,76.00]
-plt.hist(data,density=False)
-plt.axis([0, 100, 0, 5]) 
-plt.ylabel('수')
-plt.xlabel('몸무게')
+data = np.array([60.00,75.00,88.00,74.00,
+                73.00,92.00,80.00,97.00,
+                83.00,71.00,68.00,76.00])
+
+def getRange(data):
+    return int(np.amax(data)-np.amin(data))
+
+def getProperC(data):
+    return int(math.log(data.size)/math.log(2))+1
+
+showGrouped = False
+
+binValue = getProperC(data) if showGrouped else getRange(data)
+arr=plt.hist(data,bins=binValue, weights = np.ones_like(data)/ (data.size))
+
+for i in range(binValue):
+    plt.text(arr[1][i],arr[0][i],str(round(arr[0][i]*100,2))+"%")
+
+plt.ylabel('numbers')
+plt.xlabel('weight')
+
 plt.show()
